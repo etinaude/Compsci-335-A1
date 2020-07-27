9f6773a19d91c0eb4cce2960068efe018ef8472d
etinaude <etinaude@gmail.com>
2020-07-28 00:22:44 +1200

    big update

--- enau831.js
+++ enau831.js
@@ -1,1 +1,42 @@
-document.body.innerHTML = "HI";
+function run() {
+  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.send();
+  document.getElementById("here").innerHTML = "HHHH";
+}
+function get_start() {
+  var data = [];
+  var xmlHttp = new XMLHttpRequest();
+  theURL = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/htmlcomments";
+  xmlHttp.open("GET", theURL, false);
+  xmlHttp.send(null);
+  data[0] = xmlHttp.responseText;
+
+  theURL = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/items";
+  xmlHttp.open("GET", theURL, false);
+  xmlHttp.send(null);
+  data[1] = xmlHttp.responseText;
+
+  theURL = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/news";
+  xmlHttp.open("GET", theURL, false);
+  xmlHttp.send(null);
+  data[2] = xmlHttp.responseText;
+
+  theURL = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/user";
+  xmlHttp.open("GET", theURL, false);
+  xmlHttp.send(null);
+  data[3] = xmlHttp.responseText;
+
+  theURL = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/vcard";
+  xmlHttp.open("GET", theURL, false);
+  xmlHttp.send(null);
+  data[4] = xmlHttp.responseText;
+
+  theURL = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/version";
+  xmlHttp.open("GET", theURL, false);
+  xmlHttp.send(null);
+  data[5] = xmlHttp.responseText;
+
+  for (i in data) {
+    console.log(data[i]);
+  }
+}
