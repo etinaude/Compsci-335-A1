d7649d0bc27648ef2267e80e05cf35233c3a0e1e
etinaude <etinaude@gmail.com>
2020-08-31 23:45:08 +1200

    formatted js

--- test 1/enau831.js
+++ test 1/enau831.js
@@ -1,3 +1,4 @@
+// switch from home to staff and vise versa
 function switchHome() {
   document.getElementById("staff").style.display = "none";
   document.getElementById("home").style.display = "flex";
@@ -7,7 +8,9 @@ function switchStaff() {
   document.getElementById("staff").style.display = "flex";
 }
 
+//take information and display it in a pop up modal
 function details(upi, id, first, last, email) {
+  //check if image exists
   if (id == "undefined") {
     image = `<img
                 src="https://unidirectory.auckland.ac.nz/static/g5Km3OjLZuWCA8w7PdOyS4j603aTN0QC7X2gk6kRhEs.png"
@@ -23,6 +26,7 @@ function details(upi, id, first, last, email) {
                 id = "profile"
             />`;
   }
+  //make info string
   var info = `<div id="info">
             <h3>${first} ${last}</h3>
             <p>Email:   <a href="mailto:${email}"><div id="space"></div>${email}</a></p>
@@ -30,26 +34,33 @@ function details(upi, id, first, last, email) {
             <br />
             <a href="https://unidirectory.auckland.ac.nz/people/vcard/${upi}" download>🠗 Add Contact</a>
             </div>`;
+  //add info to modal
   document.getElementById("modalContent").innerHTML = `${info} ${image}`;
+  //show modal
   document.getElementById("modal").style.display = "block";
 }
+
+//close modal once clicked
 function closeModal() {
   document.getElementById("modal").style.display = "none";
 }
 
 function formatData(data) {
+  //create table headings
   str = `
       <tr class="row">
-      <th  id="startHead" class="T0">First name</th>
-      <th class="T1" >Last name</th>
-      <th class="T2" >UPI</th>
-      <th id="endHead" class="T3" >Email</th>
+        <th  id="startHead" class="T0">First name</th>
+        <th class="T1" >Last name</th>
+        <th class="T2" >UPI</th>
+        <th id="endHead" class="T3" >Email</th>
       <tr>
       <tbody>
       `;
   count = 0;
+  //loop through data
   data.forEach((element) => {
     console.log(element);
+    //alternate colors
     var num = 0;
     if (count % 2) {
       num = "even";
@@ -57,6 +68,7 @@ function formatData(data) {
       num = "odd";
     }
     count++;
+    //create row HTML
     str += `<tr class="${num}" onclick="details('${element["profileUrl"][0]}','${element["imageId"]}', '${element["legalFirstName"]}' ,'${element["legalLastName"]}','${element["emailAddresses"][0]}')">
         <td class="T0">${element["legalFirstName"]}</td>
         <td class="T1">${element["legalLastName"]}</td>
@@ -64,17 +76,16 @@ function formatData(data) {
         <td class="T3">${element["emailAddresses"][0]}</td>
     </tr>`;
   });
+  //end table and add to the document
   str += "</tbody>";
   document.getElementById("results").innerHTML = str;
 }
 
+//get api through proxy
 function searchStaff() {
+  // percent formatted url
   var URL =
     "https%3A%2F%2Funidirectory.auckland.ac.nz%2Frest%2Fsearch%3ForgFilter%3DMATHS";
-  //var URL =
-  //"https%3A%2F%2Funidirectory.auckland.ac.nz%2Fpeople%2Fimageraw%2Fsgal018%2F10300384%2Fbiggest ";
-  //https://unidirectory.auckland.ac.nz/people/imageraw/sgal018/10300384/biggest
-
   fetch(`https://dividni.com/cors/CorsProxyService.svc/proxy?url=${URL}`)
     .then((response) => response.json())
     .then((data) => formatData(data["list"]));
