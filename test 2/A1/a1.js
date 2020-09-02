8e2d66b5243f819a04729cb64092a5193efd0b40
etinaude <etinaude@gmail.com>
2020-09-03 10:24:58 +1200

    tabs

---
+++ test 2/A1/a1.js
@@ -0,0 +1,217 @@
+function format_table(data) {
+  str = `
+    <tr>
+    <th class="T0">ID</th>
+    <th class="T1" >Origin</th>
+    <th class="T2" >Price</th>
+    <th class="T3" >Item</th>
+    <th class="T4" >Type</th>
+    <tr>
+    `;
+  var items = [];
+  for (var i = 0; i < data.length; i++) {
+    items[i] = [
+      data[i].getElementsByTagName("ItemId")[0].childNodes[0].nodeValue,
+      data[i].getElementsByTagName("Origin")[0].childNodes[0].nodeValue,
+      data[i].getElementsByTagName("Price")[0].childNodes[0].nodeValue,
+      data[i].getElementsByTagName("Title")[0].childNodes[0].nodeValue,
+      data[i].getElementsByTagName("Type")[0].childNodes[0].nodeValue,
+    ];
+    str += `<tr class="line" id="${items[i][0]}" onclick="product_image(${items[i][0]})">`;
+    for (var j = 0; j < 5; j++) {
+      str += `<td class="T${j}">
+          ${items[i][j]}
+          </td>`;
+    }
+    str += `</tr>`;
+    document.getElementById("results").innerHTML = str;
+  }
+  document.getElementById("here").innerHTML = `
+          <img class="main_image" src="http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/itemimg?id=${items[0][0]}">
+          <img class="main_image" src="http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/itemimg?id=${items[1][0]}">
+        `;
+}
+function show_image(data) {}
+function product_image(image) {
+  document.getElementById("product_image").innerHTML = `
+  <img class="product_image" onclick="close_img()" src="http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/itemimg?id=${image}">
+  </img>
+  `;
+  document.getElementById("product_image").style.display = "block";
+}
+function close_img() {
+  document.getElementById("product_image").style.display = "none";
+}
+function format_comments(data) {
+  //data.replace("</p>", "");
+  data = data.split("<p>");
+  data[0] = null;
+  for (var i = 1; i < data.length; i++) {
+    data[i] = `<p class="comment">` + data[i];
+  }
+  str = "";
+  for (var i = 1; i < data.length; i++) {
+    str += data[i];
+  }
+  document.getElementById("comment_cont").innerHTML = str;
+}
+function submit_comment() {
+  var name = "inconspicuous name";
+  document.getElementById("comment_cont").innerHTML = `
+  <p class="comment"><em>${
+    document.getElementById("comment_input").value
+  }</em> — <b>${name}</b></p>
+  ${document.getElementById("comment_cont").innerHTML}`;
+  fetch(
+    `http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/comment?name=${name}`,
+    {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(`${document.getElementById("comment_input").value}`),
+    }
+  ).then(() => {
+    console.log("posted");
+  });
+}
+
+function format_news(data) {
+  var info = [[], []];
+  for (var i = 0; i < data.length; i++) {
+    data[i].innerHTML.replace("<descriptionField>", "");
+    data[i].innerHTML.replace("<enclosurefield>", "");
+    data[i].innerHTML.replace("<guidfield>", "");
+    data[i].innerHTML.replace("<linkfield>", "");
+    data[i].innerHTML.replace("<titlefield>", "");
+  }
+  for (var i = 0; i < data.length; i++) {
+    info[i][0] = data[i].innerHTML.split("</descriptionField>");
+    info[i][1] = data[i].innerHTML.split("</enclosurefield>");
+    info[i][2] = data[i].innerHTML.split("</guidfield>");
+    info[i][3] = data[i].innerHTML.split("</linkfield>");
+    info[i][4] = data[i].innerHTML.split("</titlefield>");
+  }
+}
+
+function get_start() {
+  var data = [];
+  var xmlHttp = new XMLHttpRequest();
+  //#region Comments
+  theURL = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/htmlcomments";
+  xmlHttp.open("GET", theURL, false);
+  xmlHttp.send(null);
+  data[0] = xmlHttp.responseText;
+
+  //#endregion
+  //#region items
+  theURL = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/items";
+  xmlHttp.open("GET", theURL, false);
+  xmlHttp.send(null);
+  data[1] = xmlHttp.responseXML;
+  //#endregion
+
+  //#region news
+  var info = [];
+  fetch("http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/news", {
+    headers: new Headers({
+      Accept: "application/json",
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => (info = data))
+    .then(() => {
+      var s = "";
+      for (var i = 0; i < info.length; i++) {
+        s += `<div href="https://www.francebleu.fr/vie-quotidienne/balades-randonnees/velo-et-fromages-des-itineraires-gourmands-desormais-disponibles-dans-un-guide-1594893284" class="box row">
+        <img class="news-img" src=${info[i].enclosureField.urlField}></img>
+        <div class="col">
+        <h4>${info[i].titleField}</h4>
+        <div class="data">${info[i].pubDateField}</div>
+        <br>
+        ${info[i].descriptionField}
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <a class="linked" href="https://www.francebleu.fr/vie-quotidienne/balades-randonnees/velo-et-fromages-des-itineraires-gourmands-desormais-disponibles-dans-un-guide-1594893284">
+        more info >></a>
+        </div>
+        </div>`;
+      }
+      document.getElementById("news").innerHTML = s;
+    });
+
+  //#region user
+  theURL = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/user";
+  xmlHttp.open("GET", theURL, false);
+  xmlHttp.send(null);
+  data[3] = xmlHttp.responseText;
+  //#endregion
+
+  //#region vcard
+  xmlHttp.open(
+    "GET",
+    "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/vcard",
+    false
+  );
+  xmlHttp.send(null);
+  data[4] = xmlHttp.responseText;
+  console.log(data[4]);
+  data[4] = data[4].split(/\r?\n/);
+  var vcard = [];
+  for (var i = 0; i < data[4].length; i++) {
+    if (data[4][i].includes("TEL")) {
+      vcard[0] = data[4][i].split("VOICE:")[1];
+    } else if (data[4][i].includes("ADR;WORK;PREF:;;")) {
+      vcard[2] = data[4][i].split("ADR;WORK;PREF:;;")[1];
+      vcard[2] = vcard[2].replace(";", "<br>");
+      vcard[2] = vcard[2].replace(";", "<br>");
+      vcard[2] = vcard[2].replace(";", "<br>");
+    } else if (data[4][i].includes("EMAIL:")) {
+      vcard[1] = data[4][i].split("EMAIL:")[1];
+    }
+  }
+  console.log(vcard);
+
+  document.getElementById("contact").innerHTML = `
+  <br>
+  <a href="tel:${vcard[0]}"> P: ${vcard[0]}</a>
+  <br>
+  <a href="mailto:${vcard[1]}">E: ${vcard[1]}</a>
+  <br>
+  ${vcard[2]}
+  <br>
+  <br>
+  `;
+
+  //#endregion
+
+  //#region version
+  theURL = "http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/version";
+  xmlHttp.open("GET", theURL, false);
+  xmlHttp.send(null);
+  data[5] = xmlHttp.responseText;
+  //#endregion
+
+  format_table(data[1].getElementsByTagName("Item"));
+  format_comments(data[0]);
+}
+function switch_tab(tab) {
+  for (var i = 0; i < 5; i++) {
+    document.getElementById(`B${i}`).style = "";
+    document.getElementById(`T${i}`).style = "display: None;";
+  }
+  document.getElementById(`B${tab}`).style = "background-color: darkgrey;";
+  document.getElementById(`T${tab}`).style = "display: True;";
+}
+function get_search() {
+  TERM = document.getElementById("search").value;
+  var xmlHttp = new XMLHttpRequest();
+  theURL = `http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/search?term=${TERM}`;
+  xmlHttp.open("GET", theURL, false);
+  xmlHttp.send(null);
+  var data = xmlHttp.responseXML;
+  format_table(data.getElementsByTagName("Item"));
+}
---
+++ test 2/enau831.js
@@ -0,0 +1,157 @@
+// switch from home to staff and vise versa
+function switchTab(tab) {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("staff").style.display = "none";
+  document.getElementById("courses").style.display = "none";
+  document.getElementById("infographics").style.display = "none";
+  document.getElementById(tab).style.display = "flex";
+}
+
+function getVcard(upi) {
+  //get more detail contact info from Vcard
+  var URL = `https%3A%2F%2Funidirectory.auckland.ac.nz%2Fpeople%2Fvcard%2F${upi}`;
+  var xmlHttp = new XMLHttpRequest();
+  //call via xmlHTTP to get raw html
+  xmlHttp.open(
+    "GET",
+    `https://dividni.com/cors/CorsProxyService.svc/proxy?url=${URL}`,
+    false
+  );
+  xmlHttp.send(null);
+  //split by line
+  var vcard = xmlHttp.responseText.split(/\r?\n/);
+
+  //generate backup strings if no phone or address is found
+  var num = "No Number";
+  var address = "No Address";
+
+  //loop through lines and find relevant info
+  vcard.forEach((element) => {
+    if (element.indexOf("TEL") !== -1) {
+      num = element.split("VOICE:")[1];
+    } else if (element.indexOf("ADR") !== -1) {
+      address = "";
+
+      //split address into lines
+      let temp = element.split("WORK:;;")[1].split(",");
+      temp.forEach((item) => {
+        address += `${item}<br />`;
+      });
+    }
+  });
+  //return phone number and address
+  return [num, address];
+}
+
+//take information and display it in a pop up modal
+function details(upi, id, first, last, email) {
+  //check if image exists
+  if (id == "undefined") {
+    image = `<img
+                src="https://unidirectory.auckland.ac.nz/static/g5Km3OjLZuWCA8w7PdOyS4j603aTN0QC7X2gk6kRhEs.png"
+                alt="Person image"
+                id = "profile"
+            />`;
+    console.error("no image");
+  } else {
+    var url = `https://unidirectory.auckland.ac.nz/people/imageraw/${upi}/${id}/biggest`;
+    image = `<img
+                src="${url}"
+                alt="Person image"
+                id = "profile"
+            />`;
+  }
+  //make info string
+  var vcard = getVcard(upi);
+  var info = `<div id="info">
+            <h3>${first} ${last}</h3>
+            <p>Email:   <a href="mailto:${email}"><div id="space"></div>${email}</a></p>
+            <p>UPI:   ${upi}</p>
+            <p>Phone: <a href="Tel:${vcard[0]}"> ${vcard[0]}</a></p>
+            <p>Address:<br/> ${vcard[1]}</p>
+            <br />
+            <a href="https://unidirectory.auckland.ac.nz/people/vcard/${upi}" download>🠗 Add Contact</a>
+            </div>`;
+  //add info to modal
+  document.getElementById("modalContent").innerHTML = `${info} ${image}`;
+  //show modal
+  document.getElementById("modal").style.display = "block";
+}
+
+//close modal once clicked
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+function formatData(data) {
+  //create table headings
+  str = `
+      <tr class="row">
+        <th  id="startHead" class="T0">First name</th>
+        <th class="T1" >Last name</th>
+        <th class="T2" >UPI</th>
+        <th id="endHead" class="T3" >Email</th>
+      <tr>
+      <tbody>
+      `;
+  count = 0;
+  //loop through data
+  data.forEach((element) => {
+    //console.log(element);
+    //alternate colors
+    var num = 0;
+    if (count % 2) {
+      num = "even";
+    } else {
+      num = "odd";
+    }
+    count++;
+    //create row HTML
+    str += `<tr class="${num}" onclick="details('${element["profileUrl"][0]}','${element["imageId"]}', '${element["legalFirstName"]}' ,'${element["legalLastName"]}','${element["emailAddresses"][0]}')">
+        <td class="T0">${element["legalFirstName"]}</td>
+        <td class="T1">${element["legalLastName"]}</td>
+        <td class="T2">${element["profileUrl"][0]}</td>
+        <td class="T3">${element["emailAddresses"][0]}</td>
+    </tr>`;
+  });
+  //end table and add to the document
+  str += "</tbody>";
+  document.getElementById("results").innerHTML = str;
+}
+
+//get api through proxy
+function searchStaff() {
+  // percent formatted url
+  var URL =
+    "https%3A%2F%2Funidirectory.auckland.ac.nz%2Frest%2Fsearch%3ForgFilter%3DMATHS";
+  fetch(`https://dividni.com/cors/CorsProxyService.svc/proxy?url=${URL}`)
+    .then((response) => response.json())
+    .then((data) => formatData(data["list"]));
+}
+
+function formatCourse(data) {
+  data.forEach((element) => {
+    console.log(element);
+    document.getElementById("courses").innerHTML += `<div class="course">
+                              <h2>${element.title}</h2>        
+                              <p class="courseID">${element.subject}${element.catalogNbr}</p>
+                              <p class="requirements">${element.rqrmntDescr}</p>
+                              <p class="description">${element.description}</p>
+                            </div>
+                            `;
+  });
+}
+
+//get api through proxy
+function searchCourses() {
+  // percent formatted url
+  var URL =
+    "https://api.test.auckland.ac.nz/service/courses/v2/courses?subject=MATHS&year=2020&size=500";
+  fetch(`https://dividni.com/cors/CorsProxyService.svc/proxy?url=${URL}`)
+    .then((response) => response.json())
+    .then((data) => formatCourse(data["data"]));
+}
+
+searchStaff();
+searchCourses();
+switchTab("home");
