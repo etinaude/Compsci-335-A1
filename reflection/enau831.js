282606d86a5f52cdfcd80125108be96d1c644c53
Etienne Naude <etinaude@gmail.com>
2020-09-13 06:01:16 +1200

    fancy graph

--- reflection/enau831.js
+++ reflection/enau831.js
@@ -1,32 +1,59 @@
 function graph(pts) {
   console.log(pts);
-  graph = `<svg id="graph"xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 400 400">
+  var width = 500;
+  var height = 500;
+  var graph = `<svg class="graph"xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 ${width + 100} ${height + 100} ">
   />`;
-  var max = 0;
+  var max = [0, 0];
   pts.forEach((e) => {
-    if (max < e[1]) {
-      max = e[1];
+    if (max[0] < e[1]) {
+      max[0] = e[1];
+    }
+    if (max[1] < e[2]) {
+      max[1] = e[2];
     }
   });
-  var text_pos = 370;
+  var text_pos = 510;
   for (let i = 0; i < pts.length; i++) {
-    //pts[i];
-    graph += `<circle
-                cx="${50 + i * 10}"
-                cy="${350 - 340 * (pts[i][1] / max)}"
-                r="2.0"
-                fill="black"
-              />
+    if (i < pts.length - 1) {
+      graph += `<line 
+                x1="${50 + (i * width) / pts.length}" 
+                y1="${height - 10 - height * (pts[i][2] / max[1])}" 
+                x2="${50 + ((i + 1) * width) / pts.length}" 
+                y2="${height - 10 - height * (pts[i + 1][2] / max[1])}" 
+                style="stroke:rgb(255,0,0);stroke-width:2" />`;
+      graph += `<line 
+                x1="${50 + (i * width) / pts.length}" 
+                y1="${height - 10 - height * (pts[i][1] / max[0])}" 
+                x2="${50 + ((i + 1) * width) / pts.length}" 
+                y2="${height - 10 - height * (pts[i + 1][1] / max[0])}" 
+                style="stroke:rgb(0,255,0);stroke-width:2" />`;
+    }
+    if (!(i % 10)) {
+      graph += `
               <text 
-                x="${50 + i * 10}" 
-                y="${text_pos}"
-                transform="rotate(90 ${50 + i * 10} ${text_pos})"
+                x="${50 + (i * width) / pts.length}" 
+                y="${height}"
+                transform="rotate(90 ${
+                  50 + (i * width) / pts.length
+                } ${height})"
                 font-size="10px"
               >
                   ${pts[i][0]}
               </text>`;
-    console.log(350 - i * 10, 350 * (pts[i][1] / max));
+      graph += `
+              <text 
+                x="${50 + (i * width) / pts.length}" 
+                y="${height}"
+                transform="rotate(90 ${
+                  50 + (i * width) / pts.length
+                } ${height})"
+                font-size="10px"
+              >
+                  ${pts[i][0]}
+              </text>`;
+    }
   }
 
   document.getElementById("graph").innerHTML = `${graph}</svg>`;
@@ -60,7 +87,7 @@ function format(info) {
       total_recoveries
     );*/
     if (date != "stat") {
-      pts.push([date, total_cases]);
+      pts.push([date, total_cases, new_cases]);
       str += `<tr>
                 <td>${date}</td>
                 <td>${total_cases}</td>
@@ -68,7 +95,7 @@ function format(info) {
               </tr>`;
     }
   }
-  graph(pts[pts.length-31,pts.length-1])
+  graph(pts);
   document.getElementById("data").innerHTML += `${str}</tbody></table>`;
 }
 
