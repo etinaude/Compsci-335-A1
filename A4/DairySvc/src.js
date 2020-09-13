3fb5ccd1e3e7c3ddd499062cb34b68a141fa7860
Etienne Naude <etinaude@gmail.com>
2020-09-14 03:34:57 +1200

    pentest

--- A4/DairySvc/src.js
+++ A4/DairySvc/src.js
@@ -1,6 +1,10 @@
 var Uurl = "";
 var Uname = "name";
 var comment = "DROP TABLE Comments;";
+var list = 
+
+
+
 /*
 fetch(`${Uurl}/DairyService.svc/news`, {
   headers: new Headers({
@@ -8,14 +12,18 @@ fetch(`${Uurl}/DairyService.svc/news`, {
   }),
 });
 */
-
-fetch(`${Uurl}/DairyService.svc/comment?name=${Uname}`, {
-  method: "POST", // *GET, POST, PUT, DELETE, etc.
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(`${comment}`),
-}).then(() => {
-  console.log("posted");
+console.log("here")
+list.forEach(element => {
+    fetch(`${Uurl}/DairyService.svc/register`, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({Address:"comment",
+                            Name: element,
+                            Password: element}  ),
+    }).then(() => {
+      console.log("posted", element);
+    });
 });
 //*/
