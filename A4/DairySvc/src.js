771df65e6e725541c4d3e936acbc5e61269a23cf
Etienne Naude <etinaude@gmail.com>
2020-09-15 00:48:32 +1200

    report

--- A4/DairySvc/src.js
+++ A4/DairySvc/src.js
@@ -1,53 +1,11 @@
-/*var Uurl = "";
-var Uname = "name";
-var comment = `hi"); DROP TABLE Comments; --`;
-var list = [{
-    "Address":`${comment}`,
-    "Name":`${comment}`,
-    "Password":"String content"
-    }]
-
-
-
-/*
-fetch(`${Uurl}/DairyService.svc/news`, {
-  headers: new Headers({
-    Accept: "application/json",
-  }),
-});
-
-console.log("here")
-list.forEach(element => {
-    fetch(`${Uurl}/DairyService.svc/register`, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(element),
-    }).then(() => {
-      console.log("posted", "\n",JSON.stringify(element),"\n", element);
-    });
-});
-//*/
-
-
 var Uurl = "";
 var Uname = "name";
-var comment = 
-`
-</em>
-yyyyyyyyyyyyyyyyyyyyyaaaaaaaaaaaaaaaaaaaaaayyyyyyyyyyyyy
-<b onload=alert("Wufff!")>click me!</b>
-`;
-var list = [{
-    "Address":`${comment}`,
-    "Name":`${comment}`,
-    "Password":"String content"
-    }]
+var comment = `DATATATATA`;
+var list = ['a']
 
 console.log("here")
 list.forEach(element => {
-    fetch(`${Uurl}/DairyService.svc/comment?name=a`, {
+    fetch(`${Uurl}/DairyService.svc/comment?name=DROP TABLE Comments`, {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       headers: {
         "Content-Type": "application/json",
@@ -58,3 +16,23 @@ list.forEach(element => {
     });
 });
 //*/
+
+
+//#region
+/*
+
+var Uurl = "";
+var comment = `<script>window.addEventListener("DOMContentLoaded", (event) => {console.log("DOM fully loaded and parsed");});</script>`
+
+console.log("here")
+fetch(`${Uurl}/DairyService.svc/comment?name=a`, {
+  method: "POST", // *GET, POST, PUT, DELETE, etc.
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(comment),
+}).then(() => {
+});
+
+//*/
+//#endregion
