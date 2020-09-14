2858e8c3bbef3915dbccc7b62e8925a28d1697bd
Etienne Naude <etinaude@gmail.com>
2020-09-15 00:35:22 +1200

    done outline of CS

--- Warm Up/C#/intro/Program.cs
+++ Warm Up/C#/intro/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Linq;
 
 namespace intro
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            int total =0;
+            string[] text = Console.ReadLine().Split(" ");
+            int[] array =new int[text.Length];
+            int [] no = new int [text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                array[i] = int.Parse(text[i]);
+            }
+            for(int i =0; i<text.Length;i++){
+                if(array[i]%2 ==0){
+                    array[i]=array[i]/2;
+                }
+            }
+            int[] q = array.Distinct().ToArray();
+
+            for(int i =0; i<q.Length;i++){
+                total+=q[i];
+            }
+            Console.WriteLine(total);
         }
     }
 }
