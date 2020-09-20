4c4ebc8d9e34c100014986f3429b5ce23204d25f
Etienne Naude <etinaude@gmail.com>
2020-09-21 01:27:38 +1200

    f# set up

---
+++ Warm Up/F#/Program.fs
@@ -0,0 +1,25 @@
+﻿//module Program
+
+//open System
+open System.IO
+
+// ...
+
+//[<EntryPoint>]
+//let main argv =
+    // ...
+    //0
+        
+
+
+
+
+
+// Learn more about F# at http://fsharp.org
+
+open System
+[<EntryPoint>]
+let readLines filePath = System.IO.File.ReadLines(filePath);;
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
---
+++ Warm Up/pyjs/pro.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const fs = require(`fs`)
+
+// ...
+
+function main () { 
+    console.log("hi")
+}
+
+main ()
+
