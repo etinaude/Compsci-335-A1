1494f8efe3c7c365db277778230a042f2d76e8d8
etienne naude <etinaude@gmail.com>
2020-09-22 15:33:26 +1200

    node

--- Warm Up/pyjs/pro.js
+++ Warm Up/pyjs/pro.js
@@ -1,12 +1,29 @@
-"use strict"
+const fs = require(`fs`);
+const { exit } = require("process");
+var stdin = process.openStdin();
 
-const fs = require(`fs`)
-
-// ...
-
-function main () { 
-    console.log("hi")
-}
-
-main ()
-
+stdin.addListener("data", function (d) {
+  var arr = d.toString().replace("\n", "").split(/\n| /);
+  arr = arr.filter((i) => {
+    return i != "";
+  });
+  //console.log(arr);
+  arr = arr.map((x) => {
+    if (isNaN(x)) {
+      console.log("*** NaN");
+      exit(0);
+    }
+    if (x < -2147483648 || x > 2147483647) {
+      console.log("*** NanInt32");
+      exit(0);
+    }
+    if (!(x % 2)) {
+      return x / 2;
+    }
+    return parseInt(x);
+  });
+  arr = arr.filter((x, i) => {
+    return arr.indexOf(x) == i;
+  });
+  console.log(arr.reduce((a, b) => a + b, 0));
+});
