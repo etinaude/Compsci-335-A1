543da45c7c5260deb780e4bc2660eb6a27c2cc97
Etienne Naude <etinaude@gmail.com>
2020-09-25 01:18:30 +1200

    C# DONE!!!

--- Warm Up/C#/intro/Program.cs
+++ Warm Up/C#/intro/Program.cs
@@ -12,12 +12,12 @@ namespace intro
             TextReader tIn = Console.In;
             try
             {
-                String[] text = tIn.ReadToEnd().Trim().Split();
-                var spaces = from i in text where ! (i.Trim()=="") select i.Trim();
-                var even = from item in spaces where int.Parse(item)%2==0 select int.Parse(item)/2;
-                var odd = from item in spaces where int.Parse(item)%2==1 select int.Parse(item);
-                even = even.Union(odd);
-                Console.WriteLine(even.Sum());
+                String[] text = tIn.ReadToEnd().Trim().Split ((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                //var spaces = from i in text where ! (i.Trim()=="") select i.Trim();
+                var odd = from item in text where (int.Parse(item)%2==1 || int.Parse(item)%2==-1) select int.Parse(item)/1;
+                var even = from item in text where int.Parse(item)%2==0 select int.Parse(item)/2;
+                //Console.WriteLine(odd);
+                Console.WriteLine((even.Union(odd)).Sum());
             }
             catch (Exception e)
             {
