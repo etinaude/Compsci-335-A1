d72affe441cd1dee52e01cd4b67542a04b365451
Etienne Naude <etinaude@gmail.com>
2020-09-25 14:21:56 +1200

    fs

--- Warm Up/F#/Program.fs
+++ Warm Up/F#/Program.fs
@@ -7,9 +7,14 @@ open System.IO
     
 let value = (stdin.ReadToEnd().Split ' ');
 //let value = (Array.filter (fun elem -> elem % 2 = 0) [| 1 .. 10|])
-let s  = value |> Array.toList
+let o  = value |> Array.toList
+
+let s = List.map (fun x -> if ((string x)="") then
+                                (string x).Trim()) o
+
+
 let newList = List.map (fun x -> x |> int) s
-let finalList = (Seq.distinct ((List.map (fun x -> x/2) (List.filter (fun x -> x % 2 = 0) newList))@List.filter (fun x -> x % 2 = 1) newList))
+let finalList = (Seq.distinct ((List.map (fun x -> x/2) (List.filter (fun x -> x % 2 = 0) newList))@List.filter (fun x -> (x % 2 = 1)||(x % 2 = -1)) newList))
 
 printfn "%d " (Seq.sum finalList)
 
