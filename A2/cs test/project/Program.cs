667b4e937dbcc01c38b73561f779881438934bb9
Etienne Naude <etinaude@gmail.com>
2020-09-29 01:41:25 +1300

    random text

--- A2/cs test/project/Program.cs
+++ A2/cs test/project/Program.cs
@@ -4,12 +4,25 @@ namespace project
 {
     class Program
     {
-        
+        private static Random random = new Random();
         static void Main(string[] args)
         {
-            String a = "abcd";
-            String b = "abcd";
-            Console.WriteLine("Hello World!");
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 -=+\"';:.,<>/\\?!@#$%^&*()_`~[]{}|";
+            String a = "aaa";
+            var b = new char[a.Length];
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                b[i] = chars[random.Next(chars.Length)];
+                if(a[i]==b[i]){
+                    Console.WriteLine("match");
+                }else{
+                    Console.WriteLine(b[i]);
+                    count++;
+                }
+            }
+            var fin = new String(b);
+            Console.WriteLine(count);
         }
     }
 }
